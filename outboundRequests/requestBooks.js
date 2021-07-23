23e13ab56bde11f7181141e29e7c6d017e03fbd3
Tasheem Hargrove <tasheem@tasheems-mbp.home>
2021-07-23 15:27:42 -0500

    Complete GET request to the bookServer.

--- outboundRequests/requestBooks.js
+++ outboundRequests/requestBooks.js
@@ -15,9 +15,32 @@ export default {
             };
     
             let req = http.request(options, (response) => {
-                if(response.statusCode === 401)
+                if(response.statusCode === 403)
                     reject(response.statusMessage);
+
+                response.setEncoding('utf-8');
+
+                response.on('data', (data) => {
+                    console.log(`Book response body: ${data}`);
+                    try {
+                        let body = JSON.parse(data);
+                        resolve(body);
+                    } catch(err) {
+                        reject('error with json parsing -> ' + err);
+                    }
+                });
+
+                response.on('end', () => {
+                    console.log('No more data in response.');
+                });
             });
+
+            req.on('error', (e) => {
+                console.error(`problem with request: ${e.message}`);
+                reject(502);
+            });
+
+            req.end();
         });
     }
 }
