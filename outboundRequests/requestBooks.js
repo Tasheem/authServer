f756a552d590dcfca6e3053883c4a25a4b6b8775
Tasheem Hargrove <tasheem@tasheems-mbp.home>
2021-07-23 14:50:44 -0500

    Add bookResource and a file for outbound requests to the bookServer

---
+++ outboundRequests/requestBooks.js
@@ -0,0 +1,23 @@
+import http from 'http';
+
+export default {
+    getBooks(origin) {
+        return new Promise((resolve, reject) => {
+            let options = {
+                host: 'localhost',
+                port: 7000,
+                path: '/api/book',
+                method: 'GET',
+                headers: {
+                    'X-Forwarded-For': `${origin}`,
+                    'Origin': 'localhost:4000'
+                }
+            };
+    
+            let req = http.request(options, (response) => {
+                if(response.statusCode === 401)
+                    reject(response.statusMessage);
+            });
+        });
+    }
+}
---
+++ routes/bookResource.js
@@ -0,0 +1,26 @@
+import requestBooks from '../outboundRequests/requestBooks';
+
+export async function getBooks(req, res) {
+    res.set({
+        'Access-Control-Allow-Origin': 'http://127.0.0.1:5500',
+        'Access-Control-Allow-Headers': 'Authorization, Content-Type'
+    })
+
+    let origin = req.get('Origin');
+
+    try {
+        let json = await requestBooks.getBooks(origin);
+        res.status(200).set('Content-Type', 'application/json').send(json);
+    } catch(err) {
+        console.error('ERROR with getBooks() call in bookResource.js: ');
+        console.error(error);
+        res.status(502).end();
+    }
+}
+
+export function preflightBooks(req, res) {
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin': 'http://127.0.0.1:5500',
+        'Access-Control-Allow-Headers': 'Authorization, Content-Type'
+    }).end();
+}
